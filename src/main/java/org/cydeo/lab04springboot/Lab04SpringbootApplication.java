a7217636f0087731a9df997af1d9133b0e3d6448
Ferhat <parhat002@gmail.com>
2024-04-07 17:45:37 +0300

    lab04

---
+++ src/main/java/org/cydeo/lab04springboot/Lab04SpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.cydeo.lab04springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab04SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cydeo/lab04springboot/Lab04SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cydeo.lab04springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab04SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
