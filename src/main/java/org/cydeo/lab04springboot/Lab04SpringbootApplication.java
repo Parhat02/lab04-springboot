13db5e38e164972ad9ea51781dbd2ee0c7dcc356
Ferhat <parhat002@gmail.com>
2024-04-07 17:46:27 +0300

    lab4

--- src/main/java/org/cydeo/lab04springboot/Lab04SpringbootApplication.java
+++ src/main/java/org/cydeo/lab04springboot/Lab04SpringbootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab04SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab04SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringbootApplication.class, args);
+    }
 
 }
